eeedbab2044af72869e460779b301c3c0dec217b
eyahya-khan <eyahyakhan@gmail.com>
2021-07-23 23:09:01 +0200

    responsive nav bar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,29 +1,104 @@
-import React,{useState} from 'react'
-import {Link} from 'react-router-dom'
-//here FaBars,IoIosLocate is comes from react-icon
-import {FaBars,FaTimes} from 'react-icons/fa'
-import {IoIosLocate} from 'react-icons/io'
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+//call react-icons
+import { HiLink } from 'react-icons/hi';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
 
 function Navbar() {
-    //useState hook for click on hamburger menu
-    const [click,setClick]=useState(false)
-    const handleClick=()=>setClick(!click)
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-    return ( 
-        <>
-        <div className="navbar">
-        <div className="navbar-container container">
-            <Link to='/' className="navbar logo">
-            <IoIosLocate/> Novice
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+    window.addEventListener('resize', showButton);
+    return {
+      //window.removeEventListener('resize', showButton)
+    }
+  }, []);
+
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <nav className='navbar'>
+          <div className='navbar-container container'>
+            <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+              <HiLink className='navbar-icon' />
+              BDLINK
             </Link>
-                <div className="menu-icon" onClick={handleClick}>
-                {click? <FaTimes/>: <FaBars/>}
-                </div>
-        </div>
-        </div>
-
-        </>
-    )
+            <div className='menu-icon' onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-item'>
+                <Link to='/aboutus' className='nav-links' onClick={closeMobileMenu}>
+                  About us
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/services'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Services
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/products'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Products
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/contacts'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Contact
+                </Link>
+              </li>
+              <li className='nav-btn'>
+                {button ? (
+                  <Link to='/sign-up' className='btn-link'>
+                    <Button buttonStyle='btn--outline'>SIGN UP</Button>
+                  </Link>
+                ) : (
+                  <Link to='/sign-up' className='btn-link'>
+                    <Button
+                      buttonStyle='btn--outline'
+                      buttonSize='btn--mobile'
+                      onClick={closeMobileMenu}
+                    >
+                      SIGN UP
+                    </Button>
+                  </Link>
+                )}
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
